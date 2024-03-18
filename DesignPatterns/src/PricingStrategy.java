4839bcf383ef842a1e024714d24d92a87bfb5d24
3360260 <72194187+3360260@users.noreply.github.com>
2024-03-18 15:01:14 +0100

    updated rental logic
    as it says on the tin

--- DesignPatterns/src/PricingStrategy.java
+++ DesignPatterns/src/PricingStrategy.java
@@ -1,8 +1,18 @@
 class PricingStrategy {
     public double calculateCost(Car car, int duration, int extraKm, String customer) {
-        double rate = car.getDailyRate();
+        double cost;
 
-        double cost = rate * duration;
+        if (duration <= 0 || duration > 60) {
+            throw new IllegalArgumentException("Invalid rental duration.");
+        }
+
+        if (duration < 7) {
+            cost = new DailyPricingStrategy().calculateCost(car, duration);
+        } else if (duration >= 7 && duration <= 29) {
+            cost = new WeeklyPricingStrategy().calculateCost(car, duration);
+        } else {
+            cost = new MonthlyPricingStrategy().calculateCost(car, duration);
+        }
 
         return cost;
     }
